79e76f64cb9ada946860d5cd13bbc89403a047bf
selimsevencan <selimsevencan@gmail.com>
2020-01-06 02:05:52 +0300

    Fixed responsive design

--- src/common/view/Pagination/index.js
+++ src/common/view/Pagination/index.js
@@ -10,8 +10,8 @@ class Pagination extends React.Component {
     } = this.props;
     let controls = [];
     let totalPageCount = totalPage / 10;
-    if (totalPageCount > 12) {
-      totalPageCount = 12;
+    if (totalPageCount > 10) {
+      totalPageCount = 10;
     }
     for (let i = 1; i <= totalPageCount; i++) {
       const baseClassName = 'pagination-controls-button';
